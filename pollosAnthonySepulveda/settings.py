eb74f7135239026540c076b5492c36dd35e08996
Nicolas Martinez Contreras <37675996+Nicolas-pc@users.noreply.github.com>
2020-11-08 13:33:46 -0300

    Revert "Revert ":rocket: feat: se integra whitenoise""
    This reverts commit 00ab5f36b8aa1bf315b2e4ac3bdbdb19cb628ac5.

--- pollosAnthonySepulveda/settings.py
+++ pollosAnthonySepulveda/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
